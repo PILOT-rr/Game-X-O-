bc2b20b0f22aa13e04af35fd204e66aff44478fc
ziad Mabrouki <ziadmbrouki@gmail.com>
2024-02-23 18:03:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Import your CSS file for styling if needed
+import Square from './Square';
 
 function App() {
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+
+  const handleLike = () => {
+    setLikes(likes + 1);
+  };
+
+  const handleDislike = () => {
+    setDislikes(dislikes + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+   <div className="like-dislike">
+      <button className="like-btn" onClick={handleLike}>Like ({likes})</button>
+      <button className="dislike-btn" onClick={handleDislike}>Dislike ({dislikes})</button>
     </div>
+    <Square/>
+
+  </>
+   
   );
 }
 
